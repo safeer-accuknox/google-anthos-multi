b5efc9f6374648639bf010bc988f384a9f75cce7
safeer <safeer>
2024-09-26 12:22:32 +0000

    configmap name match fixes v20

--- accuknox/multi-cluster-setup-helper.v2/certs/generator.sh
+++ accuknox/multi-cluster-setup-helper.v2/certs/generator.sh
@@ -3,3 +3,4 @@
 openssl genpkey -outform PEM -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out private.key
 openssl req -new -nodes -key private.key -config sans.csr -nameopt utf8 -utf8 -out certificate.crt
 cat certificate.crt | base64 -w 0 && echo
+# https://certificatetools.com/
