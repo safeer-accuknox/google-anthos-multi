96d6dd57209af008f220f776327437fce5d43fce
safeer <safeer>
2024-09-26 11:01:23 +0000

    configmap name match fixes v9

---
+++ accuknox/multi-cluster-setup-helper.v2/certs/generator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOMAIN="accuknox-multi-cluster-helper-webhook"  
+KEY_FILE="private.key"
+CERT_FILE="certificate.crt"
+DAYS=365 
+
+openssl genrsa -out $KEY_FILE 2048
+openssl req -new -x509 -key $KEY_FILE -out $CERT_FILE -days $DAYS -subj "/C=US/ST=State/L=City/O=Organization/CN=$DOMAIN"
+echo "caBundle: "
+cat $CERT_FILE | base64 -w 0 && echo
