aba58c0f6aca2175e59cfa98779add0c7f41f104
safeer <safeer>
2024-09-26 09:48:40 +0000

    configmap name match fixes

--- accuknox/multi-cluster-setup-helper.v2/main.py
+++ accuknox/multi-cluster-setup-helper.v2/main.py
@@ -5,44 +5,47 @@ import json
 
 app = FastAPI()
 
-cluster_name = "testing"
+cluster_name = 'testing'
 
 def create_patch(existing: bool) -> str:
     patch_operation = {
-        "op": "replace" if existing else "add",
-        "path": "/data/CLUSTERNAME",
-        "value": cluster_name
+        'op': 'replace' if existing else 'add',
+        'path': '/data/CLUSTERNAME',
+        'value': cluster_name
     }
     patch_operations = [patch_operation]
     return base64.b64encode(json.dumps(patch_operations).encode()).decode()
 
-def admission_review(uid: str, existing: bool) -> dict:
-    return {
-        "apiVersion": "admission.k8s.io/v1",
-        "kind": "AdmissionReview",
-        "response": {
-            "uid": uid,
-            "allowed": True,
-            "patchType": "JSONPatch",
-            "patch": create_patch(existing),
+def admission_review(uid: str, existing: bool, matched: bool) -> dict:
+    result = {
+        'apiVersion': 'admission.k8s.io/v1',
+        'kind': 'AdmissionReview',
+        'response': {
+            'uid': uid,
+            'allowed': True
         },
     }
+    if not matched:
+        result['response']['patchType'] = 'JSONPatch'
+        result['response']['patch'] = create_patch(existing),
 
-@app.post("/mutate")
+    return 
+
+@app.post('/mutate')
 async def mutate_request(request: dict = Body(...)):
-    print("mutation: STARTED")
+    print('mutation: STARTED')
     configmap_name = request['request']['object']['metadata']['name']
-    uid = request["request"]["uid"]
+    uid = request['request']['uid']
+    existing = False
 
-    if configmap_name in ["onboarding-vars"]:
-        data = request["request"]["object"].get("data", {})
-        existing = "CLUSTERNAME" in data
-        print("mutation", data)
-        print("mutation", JSONResponse(content=admission_review(uid, existing)))
-        return JSONResponse(content=admission_review(uid, existing))
+    if configmap_name in ['onboarding-vars']:
+        data = request['request']['object'].get('data', {})
+        existing = 'CLUSTERNAME' in data
+        print('mutation', data)
+        print('mutation', JSONResponse(content=admission_review(uid, existing)))
+        return JSONResponse(content=admission_review(uid, existing, False))
+    return JSONResponse(content=admission_review(uid, existing, True))
 
-    return JSONResponse(content={"response": {"uid": uid, "allowed": True}})
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host='0.0.0.0', port=5000)
