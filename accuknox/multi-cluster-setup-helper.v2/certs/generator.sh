ff9f65f8d25d1aa62fbcb1291884094b22086289
safeer <safeer>
2024-09-26 11:35:18 +0000

    configmap name match fixes v13

--- accuknox/multi-cluster-setup-helper.v2/certs/generator.sh
+++ accuknox/multi-cluster-setup-helper.v2/certs/generator.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-DOMAIN="accuknox-multi-cluster-helper-webhook.agents.svc"  
-KEY_FILE="private.key"
-CERT_FILE="certificate.crt"
-DAYS=365 
+export APP="accuknox-multi-cluster-helper-webhook"
+export NAMESPACE="agents"
+export CSR_NAME="${APP}.${NAMESPACE}.svc"
 
-openssl genrsa -out $KEY_FILE 2048
-openssl req -new -x509 -key $KEY_FILE -out $CERT_FILE -days $DAYS -subj "/C=US/ST=State/L=City/O=Organization/CN=$DOMAIN"
+openssl genrsa -out ${APP}.key 2048
+
+cat >csr.conf<<EOF
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = ${APP}
+DNS.2 = ${APP}.${NAMESPACE}
+DNS.3 = ${CSR_NAME}
+DNS.4 = ${CSR_NAME}.cluster.local
+EOF
+openssl req -new -key ${APP}.key -subj "/CN=${CSR_NAME}" -out ${APP}.csr -config csr.conf
+
+
+
+mv ${APP}.key private.key
+mv ${APP}.csr certificate.csr
 echo "caBundle: "
-cat $CERT_FILE | base64 -w 0 && echo
+cat certificate.csr | base64 -w 0 && echo
