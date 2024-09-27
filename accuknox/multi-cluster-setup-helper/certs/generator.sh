c4bf69b1b444506b88b6aaab43b155aefa2cc19a
safeer <safeer>
2024-09-27 02:39:18 +0000

    new certs

--- accuknox/multi-cluster-setup-helper/certs/generator.sh
+++ accuknox/multi-cluster-setup-helper/certs/generator.sh
@@ -7,4 +7,3 @@ openssl req -new -nodes -key private.key -config csrconfig.txt -nameopt utf8 -ut
 openssl req -x509 -nodes -in cert.csr -days 365 -key private.key -config certconfig.txt -extensions req_ext -nameopt utf8 -utf8 -out certificate.crt
 rm cert.csr
 cat certificate.crt | base64 -w 0 && echo
-# https://certificatetools.com/
